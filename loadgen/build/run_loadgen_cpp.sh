36af68a501c4e8da735ed6d717fca97e5da7d8dc
Omar Naman <onaman@uwaterloo.ca>
2022-05-11 15:54:32 -0400

    Pass Dataset ID to be downloaded from blob storage

--- loadgen/build/run_loadgen_cpp.sh
+++ loadgen/build/run_loadgen_cpp.sh
@@ -1,7 +1,7 @@
 source ./setup_tc.sh $@
 
 set -- ${NONTC[@]}
-# SUT_address SUT_port Storage_address Storage_port File_storage_address File_storage_port Experiment_name Selector Config_ID
+# SUT_address SUT_port Storage_address Storage_port File_storage_address File_storage_port Experiment_name Selector Config_ID Dataset_ID
 sut_address=$1;
 sut_port=$2;
 storage_address=$3;
@@ -11,11 +11,14 @@ file_storage_port=$6;
 eid=$7;
 selector=$8;
 config_id=$9;
-shift 9;
+dataset_id=${10};
+shift 10;
 output=`pwd`/cppimp/build
 
 wget "http://$file_storage_address:$file_storage_port/$config_id" --output-document=cppimp/build/mlperf.conf
 
-cd cppimp/build && ./loadgen SingleStream $sut_address $sut_port && \
+wget "http://$file_storage_address:$file_storage_port/$dataset_id$" --output-document=cppimp/build/dataset.tar.gz && \
+tar -xzf dataset.tar.gz cppimp/build/
+
 \
 cd ../../ && python3 store_results.py "$eid" "$selector" "$output" "http://$storage_address:$storage_port"
--- loadgen/build/run_loadgen_py.sh
+++ loadgen/build/run_loadgen_py.sh
@@ -1,7 +1,7 @@
 source ./setup_tc.sh $@
 
 set -- ${NONTC[@]}
-# SUT_address SUT_port Storage_address Storage_port File_storage_address File_storage_port Experiment_name Selector Config_ID
+# SUT_address SUT_port Storage_address Storage_port File_storage_address File_storage_port Experiment_name Selector Config_ID Dataset_ID
 sut_address=$1;
 sut_port=$2;
 storage_address=$3;
@@ -10,11 +10,15 @@ file_storage_address=$5;
 file_storage_port=$6;
 eid=$7;
 selector=$8;
-config_id=$9;
-shift 9;
+config_id=$9;\
+dataset_id=${10};
+shift 10;
 output=`pwd`/output/onnxruntime-cpu/ssd-mobilenet
 
 wget "http://$file_storage_address:$file_storage_port/$config_id" --output-document=cppimp/build/mlperf.conf
+wget "http://$file_storage_address:$file_storage_port/$dataset_id$" --output-document=cppimp/build/dataset.tar.gz && \
+cd python/py_loadgen/ && \
+tar -xzf dataset.tar.gz 
 
 python3 python/py_loadgen/main.py \
  --backend onnxruntime \
