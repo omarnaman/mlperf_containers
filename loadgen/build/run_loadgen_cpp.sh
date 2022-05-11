1429a602f9b21e75024bd59e1b0ec73bcf36c398
Omar Naman <onaman@uwaterloo.ca>
2022-05-11 16:13:53 -0400

    Rearrage the args to make the eid and the selector the first two

--- loadgen/build/run_loadgen_cpp.sh
+++ loadgen/build/run_loadgen_cpp.sh
@@ -1,16 +1,16 @@
 source ./setup_tc.sh $@
 
 set -- ${NONTC[@]}
-# SUT_address SUT_port Storage_address Storage_port File_storage_address File_storage_port Experiment_name Selector Config_ID Dataset_ID
-sut_address=$1;;
-storage_address=$3;
+# Experiment_name Selector SUT_address Storage_address File_storage_address Config_ID Dataset_ID Scenario
+eid=$1;
+selector=$2;
+sut_address=$3;;
+storage_address=$4;
 file_storage_address=$5;
-eid=$7;
-selector=$8;
-config_id=$9;
-dataset_id=${10};
-scenario=${11};
-shift 10;
+config_id=$6;
+dataset_id=$7;
+scenario=$8;
+shift 8;
 output=`pwd`/cppimp/build
 
 wget "http://$file_storage_address/$config_id" --output-document=cppimp/build/mlperf.conf
