1aadfc2c28e8e4afcba43a8c842b02e59026ecf9
Omar Naman <onaman@uwaterloo.ca>
2022-06-27 16:58:56 -0400

    Add model_url requirement to SUT starter

--- SUT/build/run_sut.sh
+++ SUT/build/run_sut.sh
@@ -1,4 +1,10 @@
 source ./setup_tc.sh $@
+# args: Model_URl tar.gz_file
+model_url=$1
+shift;
+
+wget $model_url -O model.tar.gz
+tar -xzf model.tar.gz -C ./model
 
 set -- ${NONTC[@]}
-python3 ./python/main.py $@
+python3 ./python/main.py $@ --model-path=model
