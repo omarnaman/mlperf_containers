240d2a6c487eba767e512e84217cebe9a30337e1
Omar Naman <onaman@uwaterloo.ca>
2022-06-27 17:49:56 -0400

    Fix a bug causing SUT args to still include model url

--- SUT/build/run_sut.sh
+++ SUT/build/run_sut.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
 source ./setup_tc.sh $@
+set -- ${NONTC[@]}
 # args: Model_URl tar.gz_file
 model_url=$1
-shift;
+shift 1;
 
 wget $model_url -O model.tar.gz
-tar -xzf model.tar.gz -C ./model
+tar -xzf model.tar.gz
 
 python3 ./python/main.py $@
