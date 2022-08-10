8424928fdc3144e612354bfe4b81595288a9cafc
Omar Naman <onaman@uwaterloo.ca>
2022-08-09 20:32:53 -0400

    Change zip-> unzip

--- SUT/Dockerfile.py
+++ SUT/Dockerfile.py
@@ -26,7 +26,7 @@ RUN cp python/*.py /workspace/build/python/
 
 # Install aws
 RUN apt-get update && \
-    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends zip curl && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends unzip curl && \
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && ./aws/install && \
     rm -rf /var/lib/apt/lists/*
 
